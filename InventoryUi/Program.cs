9fdcf667dbedfd2452fbe33884e5aa5351ede688
Mamunur Rushid <mamunurrushid60@gmail.com>
2024-10-08 10:52:40 +0600

    Ok Purchase

--- InventoryUi/Program.cs
+++ InventoryUi/Program.cs
@@ -1,30 +1,16 @@
 using InventoryUi.Extensions;
-using Microsoft.AspNetCore.Mvc.Razor;
-using Microsoft.AspNetCore.Mvc.TagHelpers;
 
-using Microsoft.AspNetCore.Mvc.Rendering;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddCustomServices(builder.Configuration);
 
 
 
-
-
-
 var app = builder.Build();
-
-
-
-
-
-
-
-
 
 
 
